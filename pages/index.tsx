f598f9bfb63a1af8bbebd73c9e6e821f88e8655e
gilliantuerah <gilliantuerah@gmail.com>
2021-08-12 17:22:25 +0700

    add at apoc fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			It&apos;s time
+			<p>Founded in 2019, Women Group is a platform for women to Learn, Share and Get Inspired from and with one and another to embrace the amazing potential of women. We’re your bestfriend to your inspiring stories and courious minds. Empowering women through gender equity to go #BeyondStandards</p>
 		</div>
 	);
 }
