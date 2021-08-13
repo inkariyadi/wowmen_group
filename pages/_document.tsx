6f213ffc78116ca7abd17d964508f3b50485b7ec
fadhilrafiii <fadhilrafi90@gmail.com>
2021-08-13 21:27:01 +0700

    add meta description content

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript, DocumentContext, } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext,) {
-		const initialProps = await Document.getInitialProps(ctx,);
-		return { ...initialProps, };
-	}
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-	render() {
-		return (
-			<Html>
-				<Head>
-					<title>WOWMEN WEBSITE</title>
-					<meta name="description" content="Wowmen is blablablablabla" />
-					<link rel="icon" href="/favicon.ico" />
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>WOWMEN WEBSITE</title>
+          <meta name="description" content="Founded in 2019, Women Group is a platform for women to Learn, Share and Get Inspired from and with one and another to embrace the amazing potential of women. We’re your bestfriend to your inspiring stories and courious minds. Empowering women through gender equity to go #BeyondStandards" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
